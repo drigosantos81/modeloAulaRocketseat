64d93d13a2ff53ef75602fe696e4f63c11f532a0
Rodrigo Emanuel <drigosantos@gmail.com>
2020-02-16 16:28:53 -0300

    Instalado nunjucks + adicinado about.html e style.css

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set("view engine", "html");
+
+nunjucks.configure("views", {
+    express:server
+})
+
+server.get("/", function(req, res) {
+    return res.render("about");
+});
+
+server.listen(5000, function() {
+    console.log("Server ir runnig");
+});

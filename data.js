b75e76e3da7f96a24b639d710fb6956d85ea83ca
Rodrigo Emanuel <drigosantos@gmail.com>
2020-02-18 12:26:17 -0300

    Criado arquivo 'data.js' e atualizado o server

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "8tTWt-JY",
+        title: "Behind the Code # 01 | Autenticação do Adonis",
+        duration: "1:06:26 min",
+        price: "Grátis",
+    },
+    {
+        id: "R7WTFLM1lto",
+        title: "Behind the Code # 02 | Recuperação de senha",
+        duration: "1:51:39 min",
+        price: "Grátis",
+    },
+    {
+        id: "7yyW7z8fbB4",
+        title: "Behind the Code # 03 | Code review App Mobile",
+        duration: "32:44 min",
+        price: "Grátis",
+    },
+    {
+        id: "sB7GaMTJ7eI",
+        title: "Behind the Code # 04 | Crud de workshops",
+        duration: "2:44:22 min",
+        price: "Grátis",
+    },
+    {
+        id: "whDe_W-CtiU",
+        title: "Behind the Code # 05 | Inscrição nos workshops",
+        duration: "1:18:23 min",
+        price: "Grátis",
+    },
+    {
+        id: "rrvgngRairM",
+        title: "Behind the Code # 06 | Feature de transição",
+        duration: "1:05:49 min",
+        price: "Grátis",
+    }
+]

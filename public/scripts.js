6fa7b8196d9ae122c49bd7fbf0a728dd167d903a
Rodrigo Emanuel <drigosantos@gmail.com>
2020-07-21 12:10:32 -0300

    Máscara de CPF OK.

--- public/scripts.js
+++ public/scripts.js
@@ -13,7 +13,7 @@ const valor = document.querySelector('input[name="valor"]');
 valor.addEventListener("keydown", function(e) {
     setTimeout(function() {
         let { value } = e.target;
-        value = value.replace(/\D/g,"");
+        value = value.replace(/\D/g, "");
 
         value = new Intl.NumberFormat('pt-BR', {
             style: 'currency',
@@ -26,27 +26,15 @@ valor.addEventListener("keydown", function(e) {
     }, 1);
 });
 
-// function format() {
-//     const input = document.querySelector('input[name="valor"]');
-//     input.addEventListener("keydown", function(e) {
-//         setTimeout(function() {
-//             let { value } = e.target;
-//             value = value.replace(/\D/g,"");
-            
-//             value = new Intl.NumberFormat('pt-BR', {
-//                 style: 'currency',
-//                 currency: 'BRL'
-//             }).format(value/100);
-
-//             e.target.value = value;
-
-//             console.log(value);
-//             document.querySelector('#resposta').innerHTML = `Valor: ${value}`;
-//         }, 1);
-//     });
-
-// }
-
-// format();
-// format();
-// format();
+const cpf = document.querySelector('input#cpf');
+console.log(cpf);
+cpf.addEventListener("keydown", function(e) {
+    setTimeout(function() {
+        let { value } = e.target;
+            value = value.replace(/\D/g, "");
+            value = value.replace(/(\d{3})(\d)/,"$1.$2")
+            value = value.replace(/(\d{3})(\d)/,"$1.$2")
+            value = value.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+        e.target.value = value;
+    });
+});
